b97a76d1a758e6b6b1213d342bc5fe802660f4ad
Myles Bulla <mylesjbulla@gmail.com>
2023-08-30 16:29:50 -0500

    minorchange to blog file

--- DevOpsFlaskedSite/blog.py
+++ DevOpsFlaskedSite/blog.py
@@ -4,6 +4,6 @@ from flask import Blueprint, render_template, url_for, request, flash, redirect
 blog = Blueprint("blog", __name__)
 
 
-@blog.route("/blog", methods=["GET"])
-def blog():
+@blog.route("/posts", methods=["GET"])
+def posts():
     return render_template('posts2.html')
