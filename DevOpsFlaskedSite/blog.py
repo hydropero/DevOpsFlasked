72908a3d6a30c43283d3cbba976d52f3c71fa7a1
Myles Bulla <mylesjbulla@gmail.com>
2023-09-18 14:02:49 -0500

    l

--- DevOpsFlaskedSite/blog.py
+++ DevOpsFlaskedSite/blog.py
@@ -17,6 +17,8 @@ def row2dict(row):
 def posts():
     posts = db.session.execute(sqla.text("SELECT * FROM formatted_post"))
     list_of_posts = []
+    posts = posts.mappings().all()
+    
     print(posts)
     return str(posts)
     [list_of_posts.append(row2dict(post)) for post in posts]
