8ddc730077aa8b82134d6686fd323fe53ca472aa
Myles Bulla <mylesjbulla@gmail.com>
2023-09-20 11:54:44 -0500

    test

--- DevOpsFlaskedSite/blog.py
+++ DevOpsFlaskedSite/blog.py
@@ -31,7 +31,7 @@ def posts():
 def blogpost():
     post_id = int(request.args.get('post_id'))
     post = db.session.execute(sqla.text(f"SELECT * FROM formatted_post WHERE id = {post_id}"))
-    post_deserialized = post.mappings().all()
+    post_deserialized = dict(post.mappings().all())
     post_deserialized.update((k, markdown.markdown(v)) for k, v in post.items() if k == "post_content")
     return render_template('blogpost.html', post=post_deserialized)
 
