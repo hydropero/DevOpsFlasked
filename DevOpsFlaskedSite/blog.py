c4660bcbc61d184483ce84424ec2e6cfe2f2fcbb
EC2 Default User <ec2-user@mylesdomain.com>
2024-03-08 21:05:07 +0000

    deploy

--- DevOpsFlaskedSite/blog.py
+++ DevOpsFlaskedSite/blog.py
@@ -55,7 +55,7 @@ def blog_post(post_id):
         # this is to ensure the variable exists regardless of whether the if statement executes
     
     post_deserialized["post_content"] = render_links(post_deserialized["post_content"]) 
-    post_deserialized.update((k, markdown.markdown(v)) for k, v in post_deserialized.items() if k == "post_content") 
+    #post_deserialized.update((k, markdown.markdown(v)) for k, v in post_deserialized.items() if k == "post_content") 
     
     return render_template('blogpost.html', post=post_deserialized)
 
